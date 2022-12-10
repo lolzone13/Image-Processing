0f09cdbc0d1a09e790d7db5b02c3c1108df4f795
lolzone13 <mohitscience123@gmail.com>
2022-12-10 15:07:51 +0530

    Added Ordered Dithering

--- main.cpp
+++ main.cpp
@@ -63,7 +63,10 @@ class Dithering : public olc::PixelGameEngine {
 
 			//Floyd_Steinberg_Dithering(m_pImage.get(), m_pDithered.get(), Quantize_RGB_nbit);
 			//JJN_Dithering(m_pImage.get(), m_pDithered.get(), Quantize_RGB_nbit);
-			Stucki_Dithering(m_pImage.get(), m_pDithered.get(), Quantize_RGB_nbit);
+			//Stucki_Dithering(m_pImage.get(), m_pDithered.get(), Quantize_RGB_nbit);
+
+			// Ordered Dithering
+			Ordered_Dithering(m_pImage.get(), m_pDithered.get(), Quantize_RGB_nbit);
 
 
 			return true;
@@ -191,6 +194,108 @@ class Dithering : public olc::PixelGameEngine {
 
 		}
 
+		vector<vector<float>> Generate_Threshold_Map(int n = 2) {
+
+			vector<vector<float>> mat = { { 0, 2}, { 3, 1} };
+
+			vector<vector<float>> fin_mat;
+			for (int i = 0; i < n; i++) {
+				// 4 * mat
+
+				vector<vector<float>> new_mat;
+
+				for (int r = 0; r < mat.size(); r++) {
+					vector<float> new_vec;
+					for (int c = 0; c < mat.size(); c++) {
+						new_vec.push_back(4.0f * mat[r][c]);
+					}
+					new_mat.push_back(new_vec);
+				}
+
+				// a00 - new_mat
+				for (int r = 0; r < mat.size(); r++) {
+					vector<float> new_vec;
+					for (int c = 0; c < mat.size(); c++) {
+						new_vec.push_back(new_mat[r][c]);
+					}
+					fin_mat.push_back(new_vec);
+				}
+	
+				// a01 - new_mat + 2
+				for (int r = 0; r < mat.size(); r++) {
+					for (int c = 0; c < mat.size(); c++) {
+						fin_mat[r].push_back(new_mat[r][c] + 2.0f);
+					}
+				}
+
+				// a10 - new_mat + 3
+				for (int r = 0; r < mat.size(); r++) {
+					vector<float> new_vec;
+					for (int c = 0; c < mat.size(); c++) {
+						new_vec.push_back(new_mat[r][c] + 3.0f);
+					}
+					fin_mat.push_back(new_vec);
+				}
+
+				// a11 - new_mat + 1
+				for (int r = mat.size(); r < 2 * mat.size(); r++) {
+					for (int c = mat.size(); c < 2 * mat.size(); c++) {
+						fin_mat[r].push_back(new_mat[r - mat.size()][c - mat.size()] + 1.0f);
+					}
+				}
+				
+				mat = fin_mat;
+				fin_mat.clear();
+			}
+			// scale by 2**(2n+2))
+
+			float scaling_factor = powf(2.0f, (float)(2*n + 2));
+			
+			for (int i = 0; i < mat.size(); i++) {
+				for (int j = 0; j < mat.size(); j++) {
+
+					//cout << mat[i][j] << " ";
+					mat[i][j] = mat[i][j] / scaling_factor;
+					mat[i][j] -= 0.5f;
+				}
+				//cout << endl;
+			}
+
+			return mat;
+
+		}
+
+		void Ordered_Dithering(const olc::Sprite* src, olc::Sprite* dest, std::function<olc::Pixel(const olc::Pixel)> funcQuantize, int n = 2) {
+
+			// threshold map
+			vector<vector<float>> threshold_map = Generate_Threshold_Map(3);
+
+			std::copy(src->pColData.begin(), src->pColData.end(), dest->pColData.begin());
+
+			// r val
+			float rVal = 255.0f / 4.0f;
+			int map_size = threshold_map.size();
+
+			olc::vi2d vPixel;
+			for (vPixel.y = 0; vPixel.y < src->height; vPixel.y++) {
+				for (vPixel.x = 0; vPixel.x < src->width; vPixel.x++) {
+					olc::Pixel op = dest->GetPixel(vPixel);
+
+					op.r += rVal * threshold_map[vPixel.y % map_size][vPixel.x % map_size];
+					op.g += rVal * threshold_map[vPixel.y % map_size][vPixel.x % map_size];
+					op.b += rVal * threshold_map[vPixel.y % map_size][vPixel.x % map_size];
+
+					olc::Pixel qp = funcQuantize(op);
+
+
+					dest->SetPixel(vPixel, qp);
+
+
+				}
+			}
+
+		}
+
 		bool OnUserUpdate(float fElapsedTime) override {
 
 			tv.HandlePanAndZoom();
@@ -210,7 +315,6 @@ class Dithering : public olc::PixelGameEngine {
 
 int main() {
 	Dithering demo;
-
 
 	if (demo.Construct(1280, 960, 1, 1))
 		demo.Start();
